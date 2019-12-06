7a74ed0095f183def1ba1148e8e3284df2459cf9
sage-jordan <sagemjrodan@gmail.com>
2019-12-05 20:11:41 -0500

    updated users db with notnullable unique etc

--- db/migrations/20191205122622_create-users-table.js
+++ db/migrations/20191205122622_create-users-table.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('users', tbl => {
-    tbl.string('username');
-    tbl.string('pass');
+    tbl.string('username', 128).unique().notNullable();
+    tbl.string('pass', 128).notNullable();
   })
 };
 
