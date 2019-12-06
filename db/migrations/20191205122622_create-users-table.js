eafd70d63372b5a10c5d5d6a95eefbbfdf32974e
sage-jordan <sagemjrodan@gmail.com>
2019-12-05 20:23:20 -0500

    added dbConfig and id to users

--- db/migrations/20191205122622_create-users-table.js
+++ db/migrations/20191205122622_create-users-table.js
@@ -1,6 +1,7 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('users', tbl => {
+    tbl.increments();
     tbl.string('username', 128).unique().notNullable();
     tbl.string('pass', 128).notNullable();
   })
